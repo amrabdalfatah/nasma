8b88d718cadcba8f3d48541b4c00ff4e0552ecb1
amr <amr@amrs-MacBook-Air.local>
2024-12-24 13:15:24 +0200

    Add Registeration User

--- nasma/urls.py
+++ nasma/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("article/", include("article.urls")),
+    path("users/", include("accounts.urls")),
     path("breathing/", include("breathing.urls")),
     path("pss/", include("pss.urls")),
 ]
--- nasma_backend/settings.py
+++ nasma_backend/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # 3-party apps
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account", 
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     # Local
     "nasma.apps.NasmaConfig", # Default App
     "accounts.apps.AccountsConfig",
@@ -50,7 +57,12 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
   "DEFAULT_PERMISSION_CLASSES": [
-    "rest_framework.permissions.AllowAny",
+        # "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
@@ -63,12 +75,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
     "http://localhost:8000",
 )
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = "nasma_backend.urls"
 
@@ -83,10 +98,15 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SITE_ID = 1 
 
 WSGI_APPLICATION = "nasma_backend.wsgi.application"
 
